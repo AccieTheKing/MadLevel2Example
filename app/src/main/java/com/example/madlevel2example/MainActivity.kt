ad40857c27e03b261c09df550af0a3d2c11b5406
Acdaling <acdaling@gmail.com>
2020-09-05 18:28:38 +0200

    Finished step 3.2
    - Implemented the addReminder method
    - Added material snackbar to show message

--- app/src/main/java/com/example/madlevel2example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2example.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -38,5 +39,22 @@ class MainActivity : AppCompatActivity() {
                 DividerItemDecoration.VERTICAL
             )
         )
+    }
+
+    /**
+     * This method will add a reminder to the list
+     */
+    private fun addReminder(reminder: String) {
+        if (reminder.isNotBlank()) {
+            reminders.add(Reminder(reminder)) // add reminder to the list of reminders
+            reminderAdapter.notifyDataSetChanged() // will apply changes
+            binding.etReminder.text?.clear() // will clear any text inside textfield if available
+        } else {
+            Snackbar.make(
+                binding.etReminder,
+                "You must fill in the input field!",
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
     }
 }
